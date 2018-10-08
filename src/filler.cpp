badc15a3045359a8b2fc8274ffc29386b80358de
svofski@gmail.com <svofski@gmail.com>
2018-10-08 15:35:04 +0300

    just rearranging filler header/cpp

--- src/filler.cpp
+++ src/filler.cpp
@@ -1,108 +1,147 @@
 #include "globaldefs.h"
 #include "filler.h"
 
-#if ZEALOUS_LOCALITY
-int PixelFiller::fill1(int clocks, int commit_time, int commit_time_pal, bool updateScreen) 
+PixelFiller::PixelFiller(Memory & _mem, IO & _io, TV & _tv): 
+    memory(_mem), io(_io), tv(_tv)
 {
-    uint32_t * bmp = this->tv.pixels();
-    int clk;
+    this->mode512 = false;
+    this->mem32 = (uint32_t *) this->memory.buffer();
+    this->pixel32 = 0;  // 4 bytes of bit planes
+    this->border_index = 0;
 
-    int ofs = this->bmpofs;
-    int raster_pixel_loc = this->raster_pixel;
-    int raster_line_loc = this->raster_line;
-    bool vborder_loc = this->vborder;
-    bool visible_loc = this->visible;
-    bool mode512_loc = this->mode512;
+    this->reset();
 
-    // clocks=16/32/48/64/80/96..
-    for (clk = 0; clk < clocks && !this->brk; clk += 2) {
-        // offset for matching border/palette writes and the raster -- test:bord2
-        const int rpixel = raster_pixel_loc - 24;
-        const bool border = vborder_loc || 
-            /* hborder */ (rpixel < (768-512)/2) || (rpixel >= (768 - (768-512)/2));
-        const int index = this->getColorIndex(rpixel, border);
-        if (clk == commit_time) {
-            this->io.commit(); // regular i/o writes (border index); test: bord2
-            mode512_loc = this->mode512;        
-        }
-        if (clk == commit_time_pal) {
-            this->io.commit_palette(index); // palette writes; test: bord2
-        }
-        if (visible_loc) {
-            const int bmp_x = raster_pixel_loc - this->center_offset;
-            if (bmp_x >= 0 && bmp_x < this->screen_width) {
-                if (mode512_loc && !border) {
-                    bmp[ofs++] = this->io.Palette(index & 0x03);
-                    bmp[ofs++] = this->io.Palette(index & 0x0c);
-                } else {
-#if TESTTABLE
-                    if (raster_line_loc & 1) {
-                        bmp[ofs++] = 0;
-                        bmp[ofs++] = 0;
-                    } else {
-                        bmp[ofs++] = 0xffffffff;
-                        bmp[ofs++] = 0xff000000;
-                    }
-#else
-                    uint32_t p = this->io.Palette(index);
-                    bmp[ofs++] = p;
-                    bmp[ofs++] = p;
+    this->io.onborderchange = [this](int border) {
+        //printf("onborderchange: %x\n", border);
+        this->border_index = border;
+    };
+
+    this->io.onmodechange = [this](bool mode) {
+        this->mode512 = mode;
+    };
+}
+
+void PixelFiller::init()
+{
+    this->first_visible_line = 312 - Options.screen_height;
+    this->center_offset = Options.center_offset;
+    this->screen_width = Options.screen_width;
+}
+
+void PixelFiller::reset()
+{
+    this->raster_pixel = 0;   // horizontal pixel counter
+    this->raster_line = 0;    // raster line counter
+    this->fb_column = 0;      // frame buffer column
+    this->fb_row = 0;         // frame buffer row
+    this->vborder = true;     // vertical border flag
+    this->visible = false;    // visible area flag
+    this->bmpofs = 0;         // bitmap offset for current pixel
+    this->brk = false;
+    this->irq = false;
+    this->pixels = this->tv.pixels();
+}
+
+#if USE_BIT_PERMUTE
+static uint32_t bit_permute_step(uint32_t x, uint32_t m, uint32_t shift) {
+    uint32_t t;
+    t = ((x >> shift) ^ x) & m;
+    x = (x ^ t) ^ (t << shift);
+    return x;
+}
 #endif
-                }
-            }
-        }
-        // 22 vsync + 18 border + 256 picture + 16 border = 312 lines
-        raster_pixel_loc += 2;
-        if (raster_pixel_loc == 768) {
-            this->brk = this->advanceLine(raster_pixel_loc, raster_line_loc,
-                    vborder_loc, visible_loc,
-                    updateScreen);
-        }
-        // load scroll register at this precise moment -- test:scrltst2
-        if (raster_line_loc == 22 + 18 && raster_pixel_loc == 150) {
-            this->fb_row = this->io.ScrollStart();
-        }
-        // irq time -- test:bord2
-        else if (raster_line_loc == 0 && raster_pixel_loc == 176) {
-            this->irq = true;
-            this->irq_clk = clk;
-        }
-    } 
 
-    this->bmpofs = ofs;
-    this->raster_pixel = raster_pixel_loc;
-    this->raster_line = raster_line_loc;
-    this->vborder = vborder_loc;
-    this->visible = visible_loc;
-    return clk;
+void PixelFiller::fetchPixels() 
+{
+    size_t addr = ((this->fb_column & 0xff) << 8) | (this->fb_row & 0xff);
+    this->pixel32 = this->mem32[0x2000 + addr];
+
+#if USE_BIT_PERMUTE
+    // h/t Code generator for bit permutations
+    // http://programming.sirrida.de/calcperm.php
+    // Input:
+    // 31 23 15 7 30 22 14 6 29 21 13 5 28 20 12 4 27 19 11 3 26 18 10 2 25 17 9 1  24 16 8 0
+    // LSB, indices refer to source, Beneš/BPC
+    uint32_t x = this->pixel32;
+    x = bit_permute_step(x, 0x00550055, 9);  // Bit index swap+complement 0,3
+    x = bit_permute_step(x, 0x00003333, 18);  // Bit index swap+complement 1,4
+    x = bit_permute_step(x, 0x000f000f, 12);  // Bit index swap+complement 2,3
+    x = bit_permute_step(x, 0x000000ff, 24);  // Bit index swap+complement 3,4
+
+    this->pixel32_grouped = x;
+#endif
 }
 
-bool PixelFiller::advanceLine(int & _raster_pixel, int & _raster_line, 
-        bool & _vborder, bool & _visible, bool updateScreen) {
-    _raster_pixel = 0;
-    _raster_line += 1;
-    this->fb_row -= 1;
-    if (!_vborder && this->fb_row < 0) {
-        this->fb_row = 0xff;
-    }
-    // update vertical border only when line changes
-    _vborder = (_raster_line < 40) || (_raster_line >= (40 + 256));
-    // turn on pixel copying after blanking area
-    _visible = _visible || 
-        (updateScreen && _raster_line == this->first_visible_line);
-    if (_raster_line == 312) {
-        _raster_line = 0;
-        _visible = false; // blanking starts
-        //printf("fill1: %d fill2: %d sum=%d\n", fill1_count, fill2_count,
-        //        fill1_count + fill2_count);
-        fill1_count = fill2_count = 0;
-        return true;
-    }
-    return false;
-}
+int PixelFiller::shiftOutPixels()
+{
+//        uint32_t p = this->pixel32;
+//        // msb of every byte in p stands for bit plane
+//        uint32_t modeless = (p >> 4 & 8) | (p >> 13 & 4) | (p >> 22 & 2) | (p >> 31 & 1);
+//        // shift left
+//        this->pixel32 = (p << 1);// & 0xfefefefe; -- unnecessary
+//        return modeless;
+#if USE_BIT_PERMUTE
+    uint32_t modeless = this->pixel32_grouped >> 28;
+    this->pixel32_grouped <<= 4;
 #else
+    uint32_t p = this->pixel32;
+    // msb of every byte in p stands for bit plane
+    uint32_t modeless = (p >> 4 & 8) | (p >> 13 & 4) | (p >> 22 & 2) | (p >> 31 & 1);
+    // shift left
+    this->pixel32 = (p << 1);// & 0xfefefefe; -- unnecessary
+#endif
+    return modeless;
+}
+
+int PixelFiller::getColorIndex(int rpixel, bool border) {
+    if (border) {
+        this->fb_column = 0;
+        return this->border_index;
+    } else {
+        if ((rpixel & 0x0f) == 0) {
+            this->fetchPixels();
+            ++this->fb_column;
+        }
+        return this->shiftOutPixels();
+    }
+}
+
+#define TESTTABLE 0
+
+int PixelFiller::fill(int clocks, int commit_time, 
+        int commit_time_pal, bool updateScreen) 
+{
+    if (TESTTABLE || commit_time || commit_time_pal || 
+            this->raster_line == 22 + 18 || 
+            this->raster_line == 0 ||
+            this->raster_line == 311 ||
+            this->raster_pixel <= (768-512)/2 + clocks ||
+            this->raster_pixel + clocks >= 768-(768-512)/2)
+    {
+        fill1_count += clocks;
+        return fill1(clocks, commit_time, commit_time_pal, updateScreen);
+    } else {
+        fill2_count += clocks;
+        if (!this->visible) {
+            this->raster_pixel += clocks;
+            return clocks;
+        }
+        else if (this->vborder) {
+            return fill4(clocks);
+        }
+        else if (this->mode512) {
+            return fill3(clocks);
+        } 
+        else {
+            return fill2(clocks);
+        }
+    }
+}
+
+
+
 int PixelFiller::fill1(int clocks, int commit_time, int commit_time_pal, bool updateScreen) {
-    uint32_t * bmp = this->tv.pixels();
+    uint32_t * bmp = this->pixels;
     int clk;
 
     for (clk = 0; clk < clocks && !this->brk; clk += 2) {
@@ -167,12 +206,11 @@ void PixelFiller::advanceLine(bool updateScreen) {
         this->brk = true;
     }
 }
-#endif
 
 /* simple fill, no out instructions underway, mode 256 */
 int PixelFiller::fill2(int clocks)
 {
-    uint32_t * const bmp = this->tv.pixels();
+    uint32_t * const bmp = this->pixels;
     int clk;
 
     int ofs = this->bmpofs;
@@ -257,7 +295,7 @@ int PixelFiller::fill2(int clocks)
 /* simple fill, no out instructions underway, mode 512 */
 int PixelFiller::fill3(int clocks)
 {
-    uint32_t * const bmp = this->tv.pixels();
+    uint32_t * const bmp = this->pixels;
     int clk;
 
     int ofs = this->bmpofs;
@@ -348,7 +386,7 @@ int PixelFiller::fill3(int clocks)
 
 int PixelFiller::fill4(int clocks)
 {
-    uint32_t * const bmp = this->tv.pixels();
+    uint32_t * const bmp = this->pixels;
     int clk;
 
     int ofs = this->bmpofs;
--- src/filler.h
+++ src/filler.h
@@ -10,8 +10,6 @@
 #include <arm_neon.h>
 #endif
 
-//#define USE_BIT_PERMUTE 1
-
 class PixelFiller
 {
 private:
@@ -33,160 +31,31 @@ private:
     uint32_t pixel32_grouped;
 #endif
     uint32_t * mem32;
+    uint32_t * pixels;
 
     Memory & memory;
     IO & io;
     TV & tv;
+
+    int fill1_count, fill2_count;
+
 public:
     bool brk;
     bool irq;
     int irq_clk;
 
 public:
-    PixelFiller(Memory & _mem, IO & _io, TV & _tv): 
-        memory(_mem), io(_io), tv(_tv)
-    {
-        this->mode512 = false;
-        this->mem32 = (uint32_t *) this->memory.buffer();
-        this->pixel32 = 0;  // 4 bytes of bit planes
-        this->border_index = 0;
+    PixelFiller(Memory & _mem, IO & _io, TV & _tv);
+    void init();
+    void reset();
+    void fetchPixels();
+    int shiftOutPixels();
+    int getColorIndex(int rpixel, bool border);
 
-        this->reset();
-
-        this->io.onborderchange = [this](int border) {
-            //printf("onborderchange: %x\n", border);
-            this->border_index = border;
-        };
-
-        this->io.onmodechange = [this](bool mode) {
-            this->mode512 = mode;
-        };
-    }
-
-    void init()
-    {
-        this->first_visible_line = 312 - Options.screen_height;
-        this->center_offset = Options.center_offset;
-        this->screen_width = Options.screen_width;
-    }
-
-    void reset()
-    {
-        this->raster_pixel = 0;   // horizontal pixel counter
-        this->raster_line = 0;    // raster line counter
-        this->fb_column = 0;      // frame buffer column
-        this->fb_row = 0;         // frame buffer row
-        this->vborder = true;     // vertical border flag
-        this->visible = false;    // visible area flag
-        this->bmpofs = 0;         // bitmap offset for current pixel
-        this->brk = false;
-        this->irq = false;
-    }
-
-    uint32_t bit_permute_step(uint32_t x, uint32_t m, uint32_t shift) {
-        uint32_t t;
-        t = ((x >> shift) ^ x) & m;
-        x = (x ^ t) ^ (t << shift);
-        return x;
-    }
-
-    void fetchPixels() 
-    {
-        size_t addr = ((this->fb_column & 0xff) << 8) | (this->fb_row & 0xff);
-        this->pixel32 = this->mem32[0x2000 + addr];
-
-#if USE_BIT_PERMUTE
-        // h/t Code generator for bit permutations
-        // http://programming.sirrida.de/calcperm.php
-        // Input:
-        // 31 23 15 7 30 22 14 6 29 21 13 5 28 20 12 4 27 19 11 3 26 18 10 2 25 17 9 1  24 16 8 0
-        // LSB, indices refer to source, Beneš/BPC
-        uint32_t x = this->pixel32;
-        x = bit_permute_step(x, 0x00550055, 9);  // Bit index swap+complement 0,3
-        x = bit_permute_step(x, 0x00003333, 18);  // Bit index swap+complement 1,4
-        x = bit_permute_step(x, 0x000f000f, 12);  // Bit index swap+complement 2,3
-        x = bit_permute_step(x, 0x000000ff, 24);  // Bit index swap+complement 3,4
-
-        this->pixel32_grouped = x;
-#endif
-    }
-
-    int shiftOutPixels()
-    {
-//        uint32_t p = this->pixel32;
-//        // msb of every byte in p stands for bit plane
-//        uint32_t modeless = (p >> 4 & 8) | (p >> 13 & 4) | (p >> 22 & 2) | (p >> 31 & 1);
-//        // shift left
-//        this->pixel32 = (p << 1);// & 0xfefefefe; -- unnecessary
-//        return modeless;
-#if USE_BIT_PERMUTE
-        uint32_t modeless = this->pixel32_grouped >> 28;
-        this->pixel32_grouped <<= 4;
-#else
-        uint32_t p = this->pixel32;
-        // msb of every byte in p stands for bit plane
-        uint32_t modeless = (p >> 4 & 8) | (p >> 13 & 4) | (p >> 22 & 2) | (p >> 31 & 1);
-        // shift left
-        this->pixel32 = (p << 1);// & 0xfefefefe; -- unnecessary
-#endif
-        return modeless;
-    }
-
-    int getColorIndex(int rpixel, bool border) {
-        if (border) {
-            this->fb_column = 0;
-            return this->border_index;
-        } else {
-            if ((rpixel & 0x0f) == 0) {
-                this->fetchPixels();
-                ++this->fb_column;
-            }
-            return this->shiftOutPixels();
-        }
-    }
-
-    int fill1_count, fill2_count;
-
-#define TESTTABLE 0
-
-    int fill(int clocks, int commit_time, int commit_time_pal, bool updateScreen) 
-    {
-        if (TESTTABLE || commit_time || commit_time_pal || 
-                this->raster_line == 22 + 18 || 
-                this->raster_line == 0 ||
-                this->raster_line == 311 ||
-                this->raster_pixel <= (768-512)/2 + clocks ||
-                this->raster_pixel + clocks >= 768-(768-512)/2)
-        {
-            fill1_count += clocks;
-            return fill1(clocks, commit_time, commit_time_pal, updateScreen);
-        } else {
-            fill2_count += clocks;
-            if (!this->visible) {
-                this->raster_pixel += clocks;
-                return clocks;
-            }
-            else if (this->vborder) {
-                return fill4(clocks);
-            }
-            else if (this->mode512) {
-                return fill3(clocks);
-            } 
-            else {
-                return fill2(clocks);
-            }
-        }
-    }
-
-
+    int fill(int clocks, int commit_time, int commit_time_pal, bool updateScreen);
     int fill1(int clocks, int commit_time, int commit_time_pal, bool updateScreen);
     int fill2(int clocks);
     int fill3(int clocks);
     int fill4(int clocks);
-    #if ZEALOUS_LOCALITY
-    bool advanceLine(int & _raster_pixel, int & _raster_line, bool & _vborder, 
-        bool & _visible, bool updateScreen);
-    #else
     void advanceLine(bool updateScreen);
-    #endif
 };
