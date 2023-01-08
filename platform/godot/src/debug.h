a8f43654bbcb0d2683c4e3057a81c5701af939e4
parallelno <mailforfriendparallelno@gmail.com>
2023-01-08 00:35:33 -0800

    breakpoint & watchpoint systems rewritten. added conditions to watchpoints. fixed a bugs

--- platform/godot/src/debug.h
+++ platform/godot/src/debug.h
@@ -1,39 +1,118 @@
 #pragma once
-#include "..\..\..\src\board.h"
+#include "..\..\..\src\memory.h"
 #include <memory>
+#include <mutex>
+#include <map>
 
 class Debug
 {
-	static const constexpr size_t MEM_BANK_SIZE = 0xffff;
-	static const constexpr size_t RAM_SIZE = MEM_BANK_SIZE;
-	static const constexpr size_t RAM_DISK_SIZE = MEM_BANK_SIZE * 4;
-	static const constexpr size_t MEMORY_SIZE = RAM_SIZE + RAM_DISK_SIZE;
+public:	
+	class Breakpoint
+	{
+	public:
 
-public:
-	Debug(Board* _boardP, Memory* _memoryP);
-	void run(const size_t _addr);
-	void read(const size_t _addr);
-	void write(const size_t _addr);
-	auto disasm(const size_t _addr, const size_t _lines, const size_t _before_addr_lines) const
-	->std::string;
+		Breakpoint(const size_t _global_addr, const bool _active = true)
+		: global_addr(_global_addr), active(_active)
+		{}
+		auto check() const -> const bool;
+		auto is_active() const -> const bool;
+		void print() const;
+
+	private:
+		size_t global_addr;
+		bool active;
+	};
+
+	class Watchpoint
+	{
+	public:
+		enum class Access : size_t
+		{
+			R,
+			W,
+			RW
+		};
+		enum class Condition : size_t
+		{
+			ANY,
+			EQU,
+			LESS,
+			GREATER,
+			LESS_EQU,
+			GREATER_EQU,
+			NOT_EQU
+		};
+
+		Watchpoint(const Access _access, const size_t _global_addr, const Condition _cond, const uint8_t _value, const bool _active = true)
+		: access(_access), global_addr(_global_addr), cond(_cond), value(_value), active(_active)
+		{}
+		auto check(const Watchpoint::Access _access, const uint8_t _value) const -> const bool;
+		auto is_active() const -> const bool;
+		void print() const;
+
+	private:
+		Access access;
+		size_t global_addr;
+		Condition cond;
+		uint8_t value;
+		bool active;
+	};
+
+	static const constexpr size_t MEM_BANK_SIZE	= 0x10000;
+	static const constexpr size_t RAM_SIZE		= MEM_BANK_SIZE;
+	static const constexpr size_t RAM_DISK_SIZE	= MEM_BANK_SIZE * 4;
+	static const constexpr size_t GLOBAL_MEM_SIZE	= RAM_SIZE + RAM_DISK_SIZE;
+
+
+	enum class AddrSpace
+	{
+		CPU, // range: [0x0000, 0xffff]
+		STACK, // range: 0x0000 - 0xFFFF accessed via stack commands: xthl, push, pop
+		GLOBAL // range: [0x0000, 0xffff] * 5 (ram + ram-disk)
+	};
+
+	Debug(Memory* _memory);
+	void init(std::function<void(const uint32_t, const uint8_t, const bool)>& debug_onread, std::function<void(const uint32_t, const uint8_t)>& debug_onwrite);
+
+	//void run(const size_t _addr);
+	void read(const size_t _global_addr, const uint8_t _val, const bool _run);
+	void write(const size_t _global_addr, const uint8_t _val);
+	auto disasm(const size_t _addr, const size_t _lines, const size_t _before_addr_lines) const ->std::string;
 	void reset();
+	void serialize(std::vector<uint8_t> &to);
+	void deserialize(std::vector<uint8_t>::iterator it, size_t size);
+
+	void add_breakpoint(const size_t _addr, const AddrSpace _addr_space = AddrSpace::CPU);
+	void del_breakpoint(const size_t _addr, const AddrSpace _addr_space = AddrSpace::CPU);
+	void add_watchpoint(const Watchpoint::Access _access, const size_t _addr, const Watchpoint::Condition _cond, const uint8_t _value, const AddrSpace _addr_space = AddrSpace::CPU);
+	void del_watchpoint(const size_t _addr, const AddrSpace _addr_space = AddrSpace::CPU);
+	void print_breakpoints();
+	void print_watchpoints();
+
+	bool check_breakpoints(const size_t _global_addr);
+	bool check_watchpoint(const Watchpoint::Access _access, const size_t _global_addr, const uint8_t _value);
+	bool check_break();
 
 private:
-    auto get_mnemonic(const uint8_t _opcode, const uint8_t _data_l, const uint8_t _data_h) const
-	-> const std::string;
-	
-	auto get_disasm_line(const size_t _addr, const uint8_t _opcode, const uint8_t _data_l, const uint8_t _data_h) const
-	->const std::string ;
-	
-	auto get_disasm_db_line(const size_t _addr, const uint8_t _data) const
-	->const std::string;
+	auto get_mnemonic(const uint8_t _opcode, const uint8_t _data_l, const uint8_t _data_h) const -> const std::string;
+	auto get_disasm_line(const size_t _addr, const uint8_t _opcode, const uint8_t _data_l, const uint8_t _data_h) const ->const std::string ;
+	auto get_disasm_db_line(const size_t _addr, const uint8_t _data) const ->const std::string;
+	auto get_cmd_len(const uint8_t _addr) const -> const size_t;
+	auto get_addr(const size_t _end_addr, const size_t _before_addr_lines) const -> size_t;
+	auto get_breakpoint_global_addr(size_t _addr, const AddrSpace _addr_space) const -> const size_t;
+	auto get_watchpoint_global_addr(size_t _addr, const AddrSpace _addr_space) const -> const size_t;
 
-	auto get_cmd_len(const uint8_t _addr) const 
-	-> const size_t;
-	
-	size_t get_addr(const size_t _end_addr, const size_t _before_addr_lines) const;
-	uint64_t mem_runs[MEMORY_SIZE];
-	uint64_t mem_reads[MEMORY_SIZE];
-	uint64_t mem_writes[MEMORY_SIZE];
+private:
+	//uint8_t  mem[GLOBAL_MEM_SIZE];
+	uint64_t mem_runs[GLOBAL_MEM_SIZE];
+	uint64_t mem_reads[GLOBAL_MEM_SIZE];
+	uint64_t mem_writes[GLOBAL_MEM_SIZE];
+
 	Memory* memoryP;
+
+	std::mutex breakpoints_mutex;
+	std::mutex watchpoints_mutex;
+	std::map<size_t, Debug::Breakpoint> breakpoints;
+	std::map<size_t, Debug::Watchpoint> watchpoints;
+	bool wp_break;
 };
